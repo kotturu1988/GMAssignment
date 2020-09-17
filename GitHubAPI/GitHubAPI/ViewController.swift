13d3450080838542670a157299e95dc1908ff34f
kotturu1988 <67432309+kotturu1988@users.noreply.github.com>
2020-09-17 11:07:00 -0400

    Adding project to repo

---
+++ GitHubAPI/GitHubAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubAPI
+//
+//  Created by DTOMac13 on 9/17/20.
+//  Copyright © 2020 AKSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
