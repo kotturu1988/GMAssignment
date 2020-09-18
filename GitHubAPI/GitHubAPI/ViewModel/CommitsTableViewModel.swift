339db758aa2a1875f13ae77940dad279abf030ef
Akarsh <kotturu1988@gmail.com>
2020-09-18 10:49:42 -0400

    Updated CommitsTableViewModel with count and cellViewModel

--- GitHubAPI/GitHubAPI/ViewModel/CommitsTableViewModel.swift
+++ GitHubAPI/GitHubAPI/ViewModel/CommitsTableViewModel.swift
@@ -11,6 +11,7 @@ import Foundation
 class CommitsTableViewModel {
     private let networking = Networking()
     private var commits : [Commit]?
+    
     public func getCommits(completion:(()->Void)?){
         
         networking.callNetwork(endpoint: GitHubAPI.repositories, type: [Commit].self){[weak self] response in
@@ -19,4 +20,15 @@ class CommitsTableViewModel {
             
         }
     }
+    
+    public func cellViewModel(index:Int) -> CommitsTableViewCellModel?{
+        guard let commits = commits else {return nil}
+        let commitsTableViewCellModel = CommitsTableViewCellModel(commit: commits[index])
+        return commitsTableViewCellModel
+    }
+    
+    public var count : Int{
+        return commits?.count ?? 0
+    }
+    
 }
