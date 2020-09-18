e883c86aee35134c8b1828aeee9f551056c4b7c2
Akarsh <kotturu1988@gmail.com>
2020-09-18 10:41:34 -0400

    Created viewModel for Commits Tableview Cell

---
+++ GitHubAPI/GitHubAPI/ViewModel/CommitsTableViewCellModel.swift
@@ -0,0 +1,30 @@
+//
+//  CommitsTableViewCellModel.swift
+//  GitHubAPI
+//
+//  Created by DTOMac13 on 9/18/20.
+//  Copyright © 2020 AKSolutions. All rights reserved.
+//
+
+import Foundation
+
+class CommitsTableViewCellModel{
+    private let commit : Commit
+    
+    init(commit:Commit) {
+        self.commit = commit
+    }
+    
+    var sha : String{
+        return commit.sha
+    }
+    var commitMessage : String{
+        return commit.commitMessage
+    }
+    var  authorName : String{
+        return commit.authorName
+    }
+    var  date : String{
+        return commit.date
+    }
+}
